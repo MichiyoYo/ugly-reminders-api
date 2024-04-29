450285b393688546796fc01f0f6527f572d797ec
Cristina Lester <cristina.lester@activision.com>
2024-04-29 09:43:43 -0700

    created express serveer

---
+++ dto/create-reminder.ts
@@ -0,0 +1,4 @@
+// DTO stands for data transfer object
+export interface CreateReminderDto {
+  title: string;
+}
---
+++ index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import remindersRouter from './rounters/reminders';
+
+const app = express();
+
+// Middleware
+app.use(express.json()); // need to add this explicitly becaue express doesn't parse json by default
+app.use('/reminders', remindersRouter); // add middleware to look at the routes
+
+app.listen(8000, () => console.log('server started!'));
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
